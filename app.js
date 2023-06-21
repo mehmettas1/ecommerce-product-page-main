b526ce75e9e8da1964069966f9a78384e463b699
mehmet <tas349873@gmail.com>
2023-06-21 20:25:17 +0300

    last commit

--- app.js
+++ app.js
@@ -115,6 +115,35 @@ function deleteItem() {
 }
 
 
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+      const lastImg = document.querySelectorAll(".selected");
+      if (lastImg) {
+        lastImg[0].classList.remove("selected");
+      }
+      image.classList.add("selected");
+      const selectedImg = document.querySelector(".selected");
+      switch (selectedImg.getAttribute("src")) {
+        case "./images/image-product-1-thumbnail.jpg":
+          mainThumbnail.src = "./images/image-product-1.jpg";
+          mainThumbnailLightBox.src = "./images/image-product-1.jpg";
+          break;
+        case "./images/image-product-2-thumbnail.jpg":
+          mainThumbnail.src = "./images/image-product-2.jpg";
+          mainThumbnailLightBox.src = "./images/image-product-2.jpg";
+          break;
+        case "./images/image-product-3-thumbnail.jpg":
+          mainThumbnail.src = "./images/image-product-3.jpg";
+          mainThumbnailLightBox.src = "./images/image-product-3.jpg";
+          break;
+        case "./images/image-product-4-thumbnail.jpg":
+          mainThumbnail.src = "./images/image-product-4.jpg";
+          mainThumbnailLightBox.src = "./images/image-product-4.jpg";
+          break;
+      }
+    });
+  });
+
   
 menuBtn.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", closeMenu);
